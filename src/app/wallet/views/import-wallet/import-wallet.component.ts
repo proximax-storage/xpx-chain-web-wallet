8b4bad5f6a8f09bc51a3abc877874b74633f3cd9
ROIMER PERAZA <rperaza@isarq.com>
2018-10-03 08:40:20 -0400

    [devep] Add dropdown network and configure create wallet, take network selected - (8h) - desblock/proxiWallet#17

--- src/app/wallet/views/import-wallet/import-wallet.component.ts
+++ src/app/wallet/views/import-wallet/import-wallet.component.ts
@@ -3,7 +3,7 @@ import { FormGroup, FormBuilder, Validators, AbstractControl } from "@angular/fo
 import { Router, ActivatedRoute } from "@angular/router";
 import { map } from "rxjs/operators";
 import { Observable } from "rxjs";
-import { Account, NetworkType, SimpleWallet, Password, EncryptedPrivateKey } from 'nem2-sdk';
+import { Account, SimpleWallet, Password, EncryptedPrivateKey } from 'nem2-sdk';
 import { AppConfig } from "../../../config/app.config";
 import { AccountsInterface, WalletAccountInterface, SharedService, WalletService } from "../../../shared";
 
@@ -16,13 +16,9 @@ import { AccountsInterface, WalletAccountInterface, SharedService, WalletService
 export class ImportWalletComponent implements OnInit {
  
   importWalletForm: FormGroup;
-  network$: Observable<string>;
-  network: number;
-  observables: Array<string> = [];
   viewCreatedWallet = 1;
   pvk: string;
   address: string;
-  red: number;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -37,12 +33,6 @@ export class ImportWalletComponent implements OnInit {
 
   ngOnInit() {
     this.importForm();
-    this.network$ = this.walletService.getNetworkObservable();
-    this.observables['network'] = this.network$.subscribe(
-      next => {
-        this.network = NetworkType[next];
-      }
-    );
   }
 
   /**
@@ -172,13 +162,5 @@ export class ImportWalletComponent implements OnInit {
     this.importWalletForm.reset();
     return;
   }
-
-
-  ngOnDestroy(): void {
-    //Called once, before the instance is destroyed.
-    //Add 'implements OnDestroy' to the class.
-    this.observables['network'].unsubscribe();
-  }
-
 }
 
