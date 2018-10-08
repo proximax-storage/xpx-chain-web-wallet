13188675fff1d33a2c24ab62c74b727673e21584
ROIMER PERAZA <rperaza@isarq.com>
2018-10-08 19:06:32 -0400

    Error correction in the header

--- src/app/header/component/header-vertical/header-vertical.component.ts
+++ src/app/header/component/header-vertical/header-vertical.component.ts
@@ -9,23 +9,15 @@ export class HeaderVerticalComponent implements OnInit {
   keyObject = Object.keys;
   @Input() header: object;
   @Input() showMenu: boolean;
-<<<<<<< HEAD
   @Output() logout: EventEmitter<any> = new EventEmitter();
-=======
   @Output() selectNetwork = new EventEmitter();
 
->>>>>>> 9b3df55775f4f1e5b2f3ddb5749c9b5e6d8356e6
   constructor() { }
 
   ngOnInit() {
   }
-<<<<<<< HEAD
+
   logoutEvent(param){
-
     this.logout.emit(param);
-
   }
-
-=======
->>>>>>> 9b3df55775f4f1e5b2f3ddb5749c9b5e6d8356e6
 }
