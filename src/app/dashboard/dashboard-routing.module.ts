4a9675f2fd254efea21d226be4ad06a96a31e65e
rperaza <roimerj63@gmail.com>
2018-10-12 01:38:48 -0400

    [devep] Design of dashboard - (3h) - desblock/proxiWallet#21 - Done

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   {
     path: `${AppConfig.routes.dashboard}`,
     component: DashboardComponent,
-    canActivate: [LoggedGuard] 
+    canActivate: [LoggedGuard]
   },
 ];
 
