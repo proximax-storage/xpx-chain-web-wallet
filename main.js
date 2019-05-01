67f585a7fd025d6a7601ec7ad0a68a382bff0595
Fenixdevblock <roimer.peraza@proximax.io>
2019-05-01 19:18:27 -0400

    Add comand to compiled desktop

--- main.js
+++ main.js
@@ -1,27 +1,32 @@
-const { app, BrowserWindow } = require('electron')
+const {
+  app,
+  BrowserWindow
+} = require('electron')
 
 let win;
 
 function createWindow() {
-   // Create the browser window.
-   //example https://angularfirebase.com/lessons/desktop-apps-with-electron-and-angular/
-   win = new BrowserWindow({
-      width: 1024,
-      height: 600,
-      backgroundColor: '#ffffff',
-      icon: `file://${__dirname}/dist/proxiWallet/assets/favicon.ico`
-   })
+  // Create the browser window.
+  //example https://angularfirebase.com/lessons/desktop-apps-with-electron-and-angular/
+  win = new BrowserWindow({
+    width: 1024,
+    height: 600,
+    backgroundColor: '#ffffff',
+    icon: `file://${__dirname}/dist/proxiWallet/assets/favicon.ico`
+  })
 
 
-    // win.loadURL(`file://${__dirname}/dist/proxiWallet/index.html`)
+  // win.loadURL(`file://${__dirname}/dist/proxiWallet/index.html`)
   win.loadURL('http://localhost:4200/')
-   //// uncomment below to open the DevTools.
-    win.webContents.openDevTools()
+  //// uncomment below to open the DevTools.
+  win.webContents.openDevTools()
 
-   // Event when the window is closed.
-   win.on('closed', function () {
-      win = null
-   })
+  // Event when the window is closed.
+  win.on('closed', function () {
+    win = null
+  })
+
+  win.loadFile('dist/proximax-web-wallet/index.html');
 }
 
 // Create window on electron intialization
@@ -30,15 +35,15 @@ app.on('ready', createWindow)
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
 
-   // On macOS specific close process
-   if (process.platform !== 'darwin') {
-      app.quit()
-   }
+  // On macOS specific close process
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
 
 app.on('activate', function () {
-   // macOS specific close process
-   if (win === null) {
-      createWindow()
-   }
+  // macOS specific close process
+  if (win === null) {
+    createWindow()
+  }
 })
