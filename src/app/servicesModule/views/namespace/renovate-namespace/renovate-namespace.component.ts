ceaf85d3b04b0e19f377f6fd2e33eb5c29abbcfd
Johandry Mora <jamdevelp@gmail.com>
2019-07-25 11:26:19 -0500

    CreateMosaic > Add duration field by time

--- src/app/servicesModule/views/namespace/renovate-namespace/renovate-namespace.component.ts
+++ src/app/servicesModule/views/namespace/renovate-namespace/renovate-namespace.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
-import { SignedTransaction } from 'tsjs-xpx-chain-sdk';
+import { SignedTransaction, MosaicId } from 'tsjs-xpx-chain-sdk';
 import { NamespacesService } from '../../../../servicesModule/services/namespaces.service';
 import { AppConfig } from '../../../../config/app.config';
 import { SharedService, WalletService } from '../../../../shared';
 import { DataBridgeService } from '../../../../shared/services/data-bridge.service';
 import { ProximaxProvider } from '../../../../shared/services/proximax.provider';
+import { TransactionsService } from 'src/app/transactions/service/transactions.service';
+import { MosaicService } from '../../../../servicesModule/services/mosaic.service';
 
 @Component({
   selector: 'app-renovate-namespace',
@@ -23,6 +25,11 @@ export class RenovateNamespaceComponent implements OnInit {
     selected: false,
     disabled: true
   }];
+  calculateRentalFee: any = '0.000000';
+  rentalFee = 100000;
+  feeType: string = 'XPX';
+  durationByBlock = '0';
+  insufficientBalance = false;
   namespaceChangeInfo: any = [];
   startHeight: number = 0;
   endHeight: number = 0;
@@ -39,13 +46,31 @@ export class RenovateNamespaceComponent implements OnInit {
     private namespaceService: NamespacesService,
     private dataBridgeService: DataBridgeService,
     private walletService: WalletService,
-    private proximaxProvider: ProximaxProvider
+    private proximaxProvider: ProximaxProvider,
+    private transactionService: TransactionsService,
+    private mosaicServices: MosaicService
   ) { }
 
   ngOnInit() {
-    this.fee = `0.0 XPX`;
+    this.fee = `0.000000 ${this.feeType}`;
     this.createForm();
     this.getNameNamespace();
+    const duration = this.renovateNamespaceForm.get('duration').value;
+    this.durationByBlock = this.transactionService.calculateDurationforDay(duration).toString();
+    this.validateRentalFee(this.rentalFee * duration);
+    this.renovateNamespaceForm.get('duration').valueChanges.subscribe(next => {
+      this.validateRentalFee(this.rentalFee * next);
+      this.durationByBlock = this.transactionService.calculateDurationforDay(next).toString();
+    });
+
+    // namespaceRoot ValueChange
+    this.renovateNamespaceForm.get('rootNamespace').valueChanges.subscribe(rootNamespace => {
+      if (rootNamespace === null || rootNamespace === undefined) {
+        this.renovateNamespaceForm.get('rootNamespace').setValue('1');
+      } else {
+        this.validateRentalFee(this.rentalFee * this.renovateNamespaceForm.get('duration').value);
+      }
+    });
   }
 
   ngOnDestroy(): void {
@@ -229,11 +254,48 @@ export class RenovateNamespaceComponent implements OnInit {
    */
   signedTransaction(common: any): SignedTransaction {
     const account = this.proximaxProvider.getAccountFromPrivateKey(common.privateKey, this.walletService.network);
-    const rootNamespaceToRenovate: string = this.renovateNamespaceForm.get('rootNamespace').value
-    const duration: number = this.renovateNamespaceForm.get('duration').value
-    const registerRootNamespaceTransaction = this.proximaxProvider.registerRootNamespaceTransaction(rootNamespaceToRenovate, this.walletService.network, duration)
+    const rootNamespaceToRenovate: string = this.renovateNamespaceForm.get('rootNamespace').value;
+    // const duration: number = parseFloat(this.durationByBlock);
+    const duration: number = 20;
+    const registerRootNamespaceTransaction = this.proximaxProvider.registerRootNamespaceTransaction(rootNamespaceToRenovate, this.walletService.network, duration);
     const signedTransaction = account.sign(registerRootNamespaceTransaction);
     return signedTransaction;
+  }
+
+  /**
+   *
+   *
+   * @param {*} amount
+   * @param {MosaicsStorage} mosaic
+   * @memberof CreateNamespaceComponent
+   */
+  validateRentalFee(amount: number) {    
+    console.log('This is a test', amount);
+    const accountInfo = this.walletService.getAccountInfo();
+    if (accountInfo !== undefined && accountInfo !== null && Object.keys(accountInfo).length > 0) {
+      if (accountInfo.mosaics.length > 0) {
+        const filtered = accountInfo.mosaics.find(element => {
+          return element.id.toHex() === new MosaicId(this.proximaxProvider.mosaicXpx.mosaicId).toHex();
+        });
+
+        const invalidBalance = filtered.amount.compact() < amount;
+        const mosaic = this.mosaicServices.filterMosaic(filtered.id);
+        this.calculateRentalFee = this.transactionService.amountFormatter(amount, mosaic.mosaicInfo);
+        if (invalidBalance && !this.insufficientBalance) {
+          this.insufficientBalance = true;
+          this.blockBtnSend = true;
+          this.renovateNamespaceForm.controls['password'].disable();
+        } else if (!invalidBalance && this.insufficientBalance) {
+          this.insufficientBalance = false;
+          this.blockBtnSend = false;
+          this.renovateNamespaceForm.controls['password'].enable();
+        }
+      } else {
+        this.insufficientBalance = true;
+        this.blockBtnSend = true;
+        this.renovateNamespaceForm.controls['password'].disable();
+      }
+    }
   }
 
 }
