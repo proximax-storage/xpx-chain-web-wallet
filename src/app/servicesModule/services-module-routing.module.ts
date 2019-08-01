67e9730005bcddef9810b155ff29498247139e33
Johandry Mora <jamdevelp@gmail.com>
2019-08-01 13:11:09 -0500

    routing explorer

--- src/app/servicesModule/services-module-routing.module.ts
+++ src/app/servicesModule/services-module-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppConfig } from "../../app/config/app.config";
-import { ServicesBoxComponent } from "./views/services-box/services-box.component"
+import { ServicesBoxComponent } from './views/services-box/services-box.component';
+import { ExplorerComponent } from './views/explorer/explorer.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,17 @@ const routes: Routes = [
       meta: {
         title: 'servicesBox.title',
         description: 'servicesBox.text',
+        override: true,
+      },
+    }
+  },
+  {
+    path: AppConfig.routes.explorer,
+    component: ExplorerComponent,
+    data: {
+      meta: {
+        title: 'explorer.title',
+        description: 'explorer.text',
         override: true,
       },
     }
