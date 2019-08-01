aafee1bdb2664e3615514d60e63cd7b4065fca2b
hachibi1 <hachibi1515@gmail.com>
2019-08-01 16:46:37 -0400

    mosaic

---
+++ src/app/servicesModule/views/mosaic/create-mosaic/create-mosaic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-mosaic',
+  templateUrl: './create-mosaic.component.html',
+  styleUrls: ['./create-mosaic.component.css']
+})
+export class CreateMosaicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
