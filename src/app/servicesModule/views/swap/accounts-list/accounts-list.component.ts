578095fbefd4ae5f6625e267b2cb5b6555a89881
Joshua Mora <joshuaalexandermora@gmail.com>
2019-09-08 15:55:41 -0500

    Changes in Swap Views

--- src/app/servicesModule/views/swap/accounts-list/accounts-list.component.ts
+++ src/app/servicesModule/views/swap/accounts-list/accounts-list.component.ts
@@ -7,23 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountsListComponent implements OnInit {
 
-  accountInfo = {
-    name: 'Selected Account',
-    address: 'VDBTDK-B55BPX-VSDQR7-AX3WX7-WFUZC3-65CTGJ-X2I5',
-    balance: '50.000'
-  };
+  accountListVisible: boolean = false;
 
   list: object[] = [
     {
-      name: 'Selected Account',
+      name: 'Elemento 1',
       address: 'VDBTDK-B55BPX-VSDQR7-AX3WX7-WFUZC3-65CTGJ-X2I5',
       balance: '50.000'
+    },
+    {
+      name: 'Elemento 2',
+      address: 'VDBTDK-B55BPX-VSDQR7-AX3WX7-WFUZC3-65CTGJ-X2I5',
+      balance: '10.000'
     }
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  changeVisibility() {
+    this.accountListVisible = !this.accountListVisible;
   }
 
 }
