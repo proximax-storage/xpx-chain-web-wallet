87f6dd1ca24fa09b1090c6012969312d3c8aff54
devyin7 <devyin7@gmail.com>
2019-09-25 17:45:06 -0400

    add new validate delete wallet

--- src/app/wallet/views/view-all-wallets/view-all-wallets.component.ts
+++ src/app/wallet/views/view-all-wallets/view-all-wallets.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/auth/services/auth.service';
 import { environment } from 'src/environments/environment';
+import { AppConfig } from 'src/app/config/app.config';
 
 @Component({
   selector: 'app-view-all-wallets',
@@ -11,13 +12,20 @@ export class ViewAllWalletsComponent implements OnInit {
   title: string;
   description: string;
   wallets: Array<any>;
-  constructor(private authService: AuthService,) { }
+
+  routes = {
+    selectTypeCreationWallet: AppConfig.routes.selectTypeCreationWallet,
+    deleteWallet: `/${AppConfig.routes.deleteWallet}/`,
+
+  };
+  constructor(private authService: AuthService, ) { }
 
   ngOnInit(
-    
-    ) {
-      this.title = 'List wallets';
-      this.wallets = this.authService.walletsOption(JSON.parse(localStorage.getItem(environment.nameKeyWalletStorage)));
-    }
+
+  ) {
+    this.title = 'Wallets available';
+    this.description = 'This are Proximax Sirius Wallet available on your device'
+    this.wallets = this.authService.walletsOption(JSON.parse(localStorage.getItem(environment.nameKeyWalletStorage)));
+  }
 
 }
