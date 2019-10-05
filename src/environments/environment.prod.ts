c105fc7f010b763e317d75e081b77926dc4e1a84
rperaza <roimerj63@gmail.com>
2019-10-04 22:37:21 -0500

    change version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,7 +9,7 @@ export const environment = {
   production: true,
   itemBooksAddress: 'sw-books-testnet-01',
   //version: '0.3.1.8',
-  version: '0.3.11.2',
+  version: '0.3.11.3',
   cacheVersion: '3',
   nameKeyBlockStorage: `sw-blocks`,
   nameKeyNodeSelected: `sw-selected-node-testnet-01`,
