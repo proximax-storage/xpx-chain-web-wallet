dfd9ccb0ad77f80ca5483098f259dcd743c86092
System Administrator <root@Luiss-MacBook-Pro-2.local>
2019-10-11 09:41:18 -0500

    UI Changes. Endpoints on four services in Home

--- src/app/home/views/home/home.component.ts
+++ src/app/home/views/home/home.component.ts
@@ -58,25 +58,25 @@ export class HomeComponent implements OnInit {
       this.services.buildStructureService(
         'Blockchain',
         true,
-        'Multisig, aggregated tx, cross chain, metadata',
+        'Multisig, aggregated tx, cross chain, metadata.',
         'icon-blockchain-full-color-80h-proximax-sirius-wallet.svg',
       ),
       this.services.buildStructureService(
         'Storage',
         true,
-        'P2P decentralised storage for any type of file',
+        'P2P decentralised storage for any type of file.',
         'icon-storage-full-color-80h-proximax-sirius-wallet.svg',
       ),
       this.services.buildStructureService(
         'Streaming',
         true,
-        'P2P decentralised streaming for video and chat',
+        'P2P decentralised streaming for video and chat.',
         'icon-streaming-full-color-80h-proximax-sirius-wallet.svg',
       ),
       this.services.buildStructureService(
         'Supercontracts',
         true,
-        'Easily modifiable digital contracts',
+        'Easily modifiable digital contracts.',
         'icon-supercontracts-full-color-80h-proximax-sirius-wallet.svg',
       )
     ];
