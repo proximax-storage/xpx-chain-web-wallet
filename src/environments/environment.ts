3ea0a49d289e031958f7002bc630da44f1462ec9
Roimer Peraza <roimerj63@gmail.com>
2019-10-25 01:26:57 -0500

    change version

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ import { ChronoUnit } from 'js-joda';
 export const environment = {
   production: false,
   itemBooksAddress: 'sw-books-mainnet',
-  version: '0.4.0',
+  version: '0.4.1',
   cacheVersion: '4-MAINNET',
   nameKeyBlockStorage: `sw-blocks`,
   nameKeyNodeSelected: `sw-selected-node-mainnet`,
