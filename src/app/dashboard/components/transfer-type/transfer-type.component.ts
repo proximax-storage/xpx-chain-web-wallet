5d9e809c1b08ddd47b2d637f4b2519d2e71bcabf
rperaza <roimerj63@gmail.com>
2019-11-13 19:56:39 -0500

    FIX BUG SWAP

--- src/app/dashboard/components/transfer-type/transfer-type.component.ts
+++ src/app/dashboard/components/transfer-type/transfer-type.component.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
-import { TransactionsInterface, TransactionsService } from '../../../transactions/services/transactions.service';
-import { MosaicService, MosaicsStorage } from 'src/app/servicesModule/services/mosaic.service';
-import { ProximaxProvider } from 'src/app/shared/services/proximax.provider';
-import { environment } from '../../../../environments/environment';
-import { SharedService } from 'src/app/shared/services/shared.service';
 import { EncryptedMessage } from 'tsjs-xpx-chain-sdk'
+import { TransactionsInterface, TransactionsService } from '../../../transactions/services/transactions.service';
+import { ProximaxProvider } from '../../../shared/services/proximax.provider';
+import { environment } from '../../../../environments/environment';
+import { SharedService } from '../../../shared/services/shared.service';
 import { WalletService } from "../../../wallet/services/wallet.service";
-import { TabHeadingDirective } from 'ng-uikit-pro-standard';
 
 @Component({
   selector: 'app-transfer-type',
@@ -46,17 +44,17 @@ export class TransferTypeComponent implements OnInit {
 
   }
 
+
   async ngOnChanges(changes: SimpleChanges): Promise<void> {
     this.searching = true;
     this.typeTransactionHex = `${this.transferTransaction.data['type'].toString(16).toUpperCase()}`;
     this.message = null;
     this.message = this.transferTransaction.data.message;
-    console.log(this.message);
-    this.hideMessage()
+    this.hideMessage();
     if (this.transferTransaction.data.transactionInfo) {
       const height = this.transferTransaction.data.transactionInfo.height.compact();
-      // console.log(typeof(height));
     }
+
     if (this.transferTransaction.data['message'].payload !== '') {
       try {
         const simple = false;
@@ -64,7 +62,7 @@ export class TransferTypeComponent implements OnInit {
         const addressAccountSimple = environment.swapAccount.addressAccountSimple;
         const addressSender = this.transferTransaction.sender.address.plain();
         if ((addressSender === addressAccountMultisig) || (addressSender === addressAccountSimple)) {
-          const msg = JSON.parse(this.transferTransaction.data['message']);
+          const msg = JSON.parse(this.transferTransaction.data['message'].payload);
           if (msg && msg['type'] === 'Swap') {
             this.msg = msg['message'];
             this.nis1hash = msg['nis1Hash'];
@@ -92,29 +90,43 @@ export class TransferTypeComponent implements OnInit {
     }
   }
 
-  changeInputType(inputType) {
-    let newType = this.sharedService.changeInputType(inputType)
+  /**
+   *
+   *
+   * @memberof TransferTypeComponent
+   */
+  async verifyRecipientInfo() {
+    let address = this.proximaxProvider.createFromRawAddress(this.transferTransaction.recipient['address']);
+    try {
+      let accountInfo = await this.proximaxProvider.getAccountInfo(address).toPromise();
+      this.recipientPublicAccount = accountInfo.publicAccount;
+    } catch (e) {}
+
+    this.senderPublicAccount = this.transferTransaction.data.signer;
+  }
+
+  /**
+   *
+   *
+   * @param {*} inputType
+   * @memberof TransferTypeComponent
+   */
+  changeInputType(inputType: string) {
+    let newType = this.sharedService.changeInputType(inputType);
     this.passwordMain = newType;
   }
 
+  /**
+   *
+   *
+   * @memberof TransferTypeComponent
+   */
   decryptMessage() {
     let common = { password: this.password }
     let firstAccount = this.walletService.currentAccount
-    // console.clear();
-    // console.log(this.transferTransaction)
-    // console.log(firstAccount);
-
-
     if (this.walletService.decrypt(common, firstAccount)) {
-      // console.log('Message', this.message);
-      // console.log('PrivateKey', common['privateKey']);
-      // console.log('RecipientPublicAccount', this.recipientPublicAccount);
-      // console.log('PanelDecrypt', this.panelDecrypt);
-      // console.log(firstAccount.address === this.recipientPublicAccount.address.address);
-
       let recipientMsg = EncryptedMessage.decrypt(this.message, common['privateKey'], this.senderPublicAccount)
       let senderMsg = EncryptedMessage.decrypt(this.message, common['privateKey'], this.recipientPublicAccount)
-
       if (firstAccount.address === this.recipientPublicAccount.address.address) {
         recipientMsg = EncryptedMessage.decrypt(this.message, common['privateKey'], this.senderPublicAccount)
         this.decryptedMessage = recipientMsg
@@ -123,43 +135,20 @@ export class TransferTypeComponent implements OnInit {
         this.decryptedMessage = senderMsg
       }
 
-      // console.log('recipient', recipientMsg);
-      // console.log('sender', senderMsg);
-
-      // if (recipientMsg.payload && recipientMsg.payload !== '') {
-      //   console.log(`/// RECIPIENT ///`)
-      //   this.decryptedMessage = recipientMsg
-      // } else if (senderMsg.payload && senderMsg.payload !== '') {
-      //   console.log(`/// SENDER ///`)
-      //   this.decryptedMessage = senderMsg
-      // }
-
-
-      // console.log('DecryptedMessage', this.decryptedMessage);
       this.panelDecrypt = 2
     } else {
-      this.sharedService.showError('','Password Invalid');
       this.panelDecrypt = 0
     }
   }
 
+  /**
+   *
+   *
+   * @memberof TransferTypeComponent
+   */
   hideMessage() {
-    this.password = ''
-    this.decryptedMessage = null
-    this.panelDecrypt = 0
+    this.password = '';
+    this.decryptedMessage = null;
+    this.panelDecrypt = 0;
   }
-
-  async verifyRecipientInfo() {
-    let address = this.proximaxProvider.createFromRawAddress(this.transferTransaction.recipient['address'])
-    try {
-      let accountInfo = await this.proximaxProvider.getAccountInfo(address).toPromise()
-      // console.log('verifyAccountRecipient', accountInfo);
-      this.recipientPublicAccount = accountInfo.publicAccount
-    } catch (e) {
-
-    }
-
-    this.senderPublicAccount = this.transferTransaction.data.signer
-  }
-
 }
