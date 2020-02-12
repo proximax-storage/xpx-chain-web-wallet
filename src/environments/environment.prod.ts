aa0acf0cbd644955f435fa0a698b7c3c548d24ef
Roimer Peraza <roimer@roimer.peraza>
2020-02-11 20:02:58 -0500

    SWAP

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -65,19 +65,18 @@ export const environment = {
     address_public_test: 'VDYN53-XXEGKK-3XHQYE-K6ZBMN-JPXN57-ZBHXA3-AW55'
   },
   nis1: {
-    // url: 'https://bctestnetswap.xpxsirius.io:7890',
-    url: 'http://192.168.2.196:7890',
+    url: 'https://bctestnetswap.xpxsirius.io:7890',
+    // url: 'http://192.168.2.196:7890',
     urlExplorer: 'http://testnet-explorer.nemtool.com/#/s_tx?hash=',
     networkType: NetworkTypes.TEST_NET,
     burnAddress: 'TBF4LAZUEJMBIOC6J24D6ZGGXE5W775TX555CTTN',
     nodes: [
-      { protocol: 'http', domain: '192.168.2.196', port: 7890 } as ServerConfig
+      { protocol: 'https', domain: 'bctestnetswap.xpxsirius.io', port: 7890 } as ServerConfig
     ],
   },
   swapAccount: {
     addressAccountMultisig: 'VAWOEOWTABXR7O3ZAK2XNA5GIBNE6PZIXDAFDWBU',
     addressAccountSimple: 'VCWAHAMZRKWU4T3MSBEV7CJVYN7TGX5ZCYMVTUJW'
-    // addressAccountSimple: 'VCWLIYQPQAJSYWMWL5BHUCA3VOWVOXZ3WTNJPTUJ'
   },
   swapAllowedMosaics: [
     { namespaceId: 'zarcade', name: 'xar', divisibility: 4 },
