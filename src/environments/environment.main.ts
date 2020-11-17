c4f583b4cbbf84b35caac54bdc9f69effb973630
Shin Neng <48303181+shinneng@users.noreply.github.com>
2020-11-17 17:28:30 +0800

    fix: bump version

--- src/environments/environment.main.ts
+++ src/environments/environment.main.ts
@@ -9,7 +9,7 @@ export const environment = {
   production: false,
   routeNodesJson: 'mainnet',
   itemBooksAddress: 'sw-books-mainnet',
-  version: '0.4.23',
+  version: '0.4.26',
   cacheVersion: '01-MAINNET',
   nameKeyBlockStorage: `sw-blocks`,
   nameKeyNodeSelected: `sw-selected-node-mainnet`,
