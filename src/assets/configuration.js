5f49130b9b1f007e11749ad31858b6c4b47e5ca8
Chun Lam <chunlam.sees@gmail.com>
2021-02-09 20:46:19 +0800

    display block height of confirmed tx, save block timestamp

--- src/assets/configuration.js
+++ src/assets/configuration.js
@@ -7,6 +7,7 @@ dynamic_configuration = {
     version: '0.5.5',
     cacheVersion: '01',
     nameKeyBlockStorage: `sw-blocks`,
+    nameKeyBlockTimestamp: `sw-blocks-timestamp`,
     nameKeyNodeSelected: `sw-selected-node-testnet`,
     nameKeyWalletStorage: `sw-testnet`,
     nameKeyNodeStorage: `sw-nodes-testnet`,
