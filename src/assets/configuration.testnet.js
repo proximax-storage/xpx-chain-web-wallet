40615c28f44906aa4d2d5add50f85af29a8220b3
Jeffersson Espinoza <devyin7@gmail.com>
2021-04-23 12:21:02 -0500

    new method update node list

--- src/assets/configuration.testnet.js
+++ src/assets/configuration.testnet.js
@@ -132,6 +132,7 @@ dynamic_configuration = {
       debug: 3
     },
     nodes:[
+      "bctestnet1.brimstone.xpxsirius.io",
       "bctestnet2.brimstone.xpxsirius.io",
       "bctestnet3.brimstone.xpxsirius.io",
       "api-1.testnet2.xpxsirius.io"
